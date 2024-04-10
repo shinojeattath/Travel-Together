7e46b160c375580e05b464b74d61ccec4186dd08
shinojeattath <109549717+shinojeattath@users.noreply.github.com>
2024-04-10 12:52:01 +0530

    review

--- travel_app/models.py
+++ travel_app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class review(models.Model):
     name = models.CharField(max_length=30)
+    place = models.CharField(max_length=30, null = True)
     review = models.CharField(max_length=300)
 
 class travelling_place(models.Model):
